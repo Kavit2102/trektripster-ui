27460e5c4c61f16b99e39f0f2fdbf9172c39eb22
Kavit2102 <kavitdesai212002@gmail.com>
2026-09-04 19:19:26 +0530

    Remove dynamic prop from ClerkProvider for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <ClerkProvider appearance={{ theme: shadcn }} dynamic>
+        <ClerkProvider appearance={{ theme: shadcn }}>
           {children}
           <Toaster />
         </ClerkProvider>
